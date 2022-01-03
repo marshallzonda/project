19cfe916ad5adf110676cbf9484f2de63de01a92
Vladislav <bond.zonda777@gmail.com>
2022-01-03 16:37:06 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Hello ${user}`);
+
+// let incr = 10,
+//     decr = 10;
+
+// ++incr;
+// --decr;
+
+
+
+// console.log(2*4 === 8);
+const isChecked = true,
+    isClose = false;
+
+    console.log(isChecked || !isClose);
